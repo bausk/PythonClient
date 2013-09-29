d96c9c8d5404979c60b6a6455a3f5d754c8a9d9c
Alex Bausk at Gibraltar <bauskas@gmail.com>
2013-09-29 15:28:08 +0300

    ZeroMQ binding added

--- AutoCADPythonClient/PythonClientCommands.cs
+++ AutoCADPythonClient/PythonClientCommands.cs
@@ -28,16 +28,7 @@ namespace AutoCADPythonClient
         //
         // NOTE: CommandMethod has overloads where you can provide helpid and
         // context menu.
-
-        // Modal Command with localized name
-        [CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)]
-        public void MyCommand() // This method can have any name
-        {
-            // Put your command code here
-
-        }
-
-
+        //THIS IS A REFERENCE PFT COMMAND OVER WHICH WE BUILD THE EXAMPLE ZEROMQ CLIENT
         [CommandMethod("PFT", CommandFlags.UsePickSet |
                                   CommandFlags.Redraw |
                                   CommandFlags.Modal)
@@ -101,48 +92,5 @@ namespace AutoCADPythonClient
                 ed.WriteMessage(ex.Message);
             }
         }
-
-
-
-
-
-        // Modal Command with pickfirst selection
-        [CommandMethod("MyGroup", "MyPickFirst", "MyPickFirstLocal", CommandFlags.Modal | CommandFlags.UsePickSet)]
-        public void MyPickFirst() // This method can have any name
-        {
-            PromptSelectionResult result = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
-            if (result.Status == PromptStatus.OK)
-            {
-                // There are selected entities
-                // Put your command using pickfirst set code here
-
-            }
-            else
-            {
-                // There are no selected entities
-                // Put your command code here
-            }
-        }
-
-        // Application Session Command with localized name
-        [CommandMethod("MyGroup", "MySessionCmd", "MySessionCmdLocal", CommandFlags.Modal | CommandFlags.Session)]
-        public void MySessionCmd() // This method can have any name
-        {
-            // Put your command code here
-        }
-
-        // LispFunction is similar to CommandMethod but it creates a lisp 
-        // callable function. Many return types are supported not just string
-        // or integer.
-        [LispFunction("MyLispFunction", "MyLispFunctionLocal")]
-        public int MyLispFunction(ResultBuffer args) // This method can have any name
-        {
-            // Put your command code here
-
-            // Return a value to the AutoCAD Lisp Interpreter
-            return 1;
-        }
-
     }
-
 }
