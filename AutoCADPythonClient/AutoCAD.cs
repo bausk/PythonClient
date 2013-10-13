0b29e6cc0979aac7fd7a652a2190af06a5b3dce2
Alex Bausk at Gibraltar <bauskas@gmail.com>
2013-10-13 21:27:59 +0300

    Before hauling class logic

---
+++ AutoCADPythonClient/AutoCAD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZeroMQ;
+
+namespace SocketWrapper
+{
+    public class PythonMessage
+        {
+            public string MessageType { get; set; }
+            public string ContentType { get; set; }
+            public object Content { get; set; }
+        }
+    public class AutoCAD
+    {
+        public void SendMessage(ZmqSocket client, PythonMessage message)
+        {
+
+        }
+        public string Meh {
+            get; set;
+        }
+    }
+}
--- AutoCADPythonClient/PythonClientCommands.cs
+++ AutoCADPythonClient/PythonClientCommands.cs
@@ -1,6 +1,7 @@
 ﻿// (C) Copyright 2013 by Microsoft 
 //
 using System;
+using Microsoft.CSharp;
 using System.Text;
 using System.Collections.Generic;
 using Autodesk.AutoCAD.Runtime;
@@ -9,21 +10,30 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
 using ZeroMQ;
+using Newtonsoft.Json;
+//using System.Runtime.Serialization;
+using SocketWrapper;
+
 
 // This line is not mandatory, but improves loading performances
 [assembly: CommandClass(typeof(AutoCADPythonClient.PythonCommands))]
 
 namespace AutoCADPythonClient
 {
-
     // This class is instantiated by AutoCAD for each document when
     // a command is called by the user the first time in the context
     // of a given document. In other words, non static data in this class
     // is implicitly per-document!
+/*    public class PythonMessage
+    {
+        public string MessageType { get; set; }
+        public string ContentType { get; set; }
+        public object Content { get; set; }
+    }
+ */
+
     public class PythonCommands
     {
-
-        ObjectIdCollection _ids = null;
         //SortedList<string, string> _blockNames = null;
 
         //Selection Set example
@@ -82,6 +92,7 @@ namespace AutoCADPythonClient
                                     ed.WriteMessage("\nSending layer information to Python try {0}...\n", requestNum);
                                     client.SendTimeout = new TimeSpan(0,0,50);
                                     client.ReceiveTimeout = new TimeSpan(0,0,50);
+                                    //JsonConvert.SerializeObject(dict)
                                     client.Send(request, Encoding.Unicode); 
                                     string reply = client.Receive(Encoding.Unicode);
                                     ed.WriteMessage("\nReceived message {0} from Python: '{1}'\n", requestNum, reply);
@@ -107,21 +118,53 @@ namespace AutoCADPythonClient
             }
         }
 
+        private ZmqSocket sendPythonMessage(string message)
+        {
+            using (ZmqContext context = ZmqContext.Create())
+            using (ZmqSocket client = context.CreateSocket(SocketType.REQ))
+            {
+                client.Connect("tcp://localhost:5556");
+                string request = message;
+                client.SendTimeout = new TimeSpan(0, 0, 50);
+                client.ReceiveTimeout = new TimeSpan(0, 0, 50);
+                client.Send(request, Encoding.Unicode);
+                //string reply = client.Receive(Encoding.Unicode);
+                return client;
+            }
+        }
+
         //OK let's try an event handler
         //http://through-the-interface.typepad.com/through_the_interface/2010/02/watching-for-deletion-of-a-specific-autocad-block-using-net.html
         //
-        [CommandMethod("LOGENT")]
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+
+        [CommandMethod("REPENT")]
         public void SetEventHandler()
         {
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Database db = doc.Database;
             Editor ed = doc.Editor;
 
-            // Ask for the name of a block to watch for
 
+            // Ask for the name of a block to watch for
+            
             PromptStringOptions pso =
               new PromptStringOptions(
-                "\nEnter entity type to log: "
+                "\nEnter entity type to mirror: "
               );
             pso.AllowSpaces = false;
 
@@ -130,8 +173,33 @@ namespace AutoCADPythonClient
                 return;
 
             string entityType = pr.StringResult.ToUpper();
+            PythonMessage message = new PythonMessage();
+            message.MessageType = "UserInput";
+            message.ContentType = "String";
+            message.Content = entityType;
 
+            string messageBlob = JsonConvert.SerializeObject(message);
+            using (ZmqContext context = ZmqContext.Create())
+            using (ZmqSocket client = context.CreateSocket(SocketType.REQ))
+            {
 
+                client.Connect("tcp://localhost:5556");
+                client.SendTimeout = new TimeSpan(0, 0, 10);
+                client.ReceiveTimeout = new TimeSpan(0, 0, 10);
+
+                client.Send(messageBlob, Encoding.Unicode);
+                string reply = client.Receive(Encoding.Unicode);
+                reply = reply.Remove(0, 1);
+                var aaa = GetString(Encoding.Convert(Encoding.Unicode, Encoding.UTF8, GetBytes(reply)));
+                //reply = Encoding.UTF8.GetString(reply.ToCharArray())
+                //PythonMessage replyMessage1 = JsonConvert.DeserializeObject<PythonMessage>(aaa);
+                PythonMessage replyMessage = JsonConvert.DeserializeObject<PythonMessage>(reply);
+                if (replyMessage.Content.Equals("OK"))
+                {
+                    ed.WriteMessage("\nIt went *okay*.\n");
+                }
+
+            }
 
         }
 
@@ -139,7 +207,7 @@ namespace AutoCADPythonClient
         {
             // Start an outer transaction that we pass to our testing
             // function, avoiding the overhead of multiple transactions
-
+            ObjectIdCollection _ids = null;
             Document doc = sender as Document;
             if (_ids != null)
             {
